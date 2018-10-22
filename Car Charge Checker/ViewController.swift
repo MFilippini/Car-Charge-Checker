cb9e06db35980854b57cbb0586c1e54f9e0c6473
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-10-22 10:05:47 -0500

    Initial Commit

---
+++ Car Charge Checker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Charge Checker
+//
+//  Created by Michael Filippini on 10/22/18.
+//  Copyright © 2018 Michael Filippini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
