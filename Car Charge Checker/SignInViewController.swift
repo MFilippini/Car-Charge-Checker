44151117e2e1c7713950d024ef9ad35c6537fe6a
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-10-30 08:37:34 -0500

    Sign in is getting closer......slowly

---
+++ Car Charge Checker/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  Car Charge Checker
+//
+//  Created by Michael Filippini on 10/30/18.
+//  Copyright © 2018 Michael Filippini. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class SignInViewController: UIViewController, GIDSignInUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
