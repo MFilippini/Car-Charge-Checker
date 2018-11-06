c9d14d8052d1a531aff1361082f822946eb109ea
Alush <5alush5b@gmail.com>
2018-11-06 14:50:59 -0600

    before test mode

--- Car Charge Checker/ViewController.swift
+++ Car Charge Checker/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var reservationDescription: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.rowHeight = 100
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+        //Return total number of chargers
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .red
+        cell.layoutSubviews()
+        return cell
     }
 
-
+    @IBAction func PlusPressed(_ sender: Any) {
+    }
+    
+    
+    
 }
 
