9bacd58a8aea08b5f5af66b8f7fd31208b5001bb
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-11-29 09:43:37 -0600

    conflicts

--- Car Charge Checker/MenuViewController.swift
+++ Car Charge Checker/MenuViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var welcomeNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func signOutClicked(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        let signIn = storyboard?.instantiateViewController(withIdentifier: "SignIn")
+        slideMenuController()?.changeMainViewController(signIn!, close: true)
+    }
     
+    @IBAction func settingsClicked(_ sender: Any) {
+    }
     
 }
