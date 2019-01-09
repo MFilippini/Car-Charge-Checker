da538104e5aa5664dd70764c270fbf6cbe06b6fe
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2019-01-09 09:57:36 -0600

    New global file

---
+++ Car Charge Checker/Globals.swift
@@ -0,0 +1,12 @@
+//
+//  Globals.swift
+//  Car Charge Checker
+//
+//  Created by Michael Filippini on 1/9/19.
+//  Copyright © 2019 Michael Filippini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var currentGroup: String? = nil
